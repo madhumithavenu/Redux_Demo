294f8225fa4719c397e73750503de33023bd3eaf
Madhumitha <madhumithavenu03@gmail.com>
2023-12-29 15:26:03 +0530

    Logger middleware

--- rtk_demo/index.js
+++ rtk_demo/index.js
@@ -2,9 +2,9 @@ const store = require('./app/store')
 const  icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
 
+
 console.log('Initial State ', store.getState())
 const unsubscribe = store.subscribe(() => {
-  console.log('Updated State ', store.getState())
 })
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
