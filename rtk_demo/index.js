5b9fb7219c4a56b26648b9503705feaed851a749
Madhumitha <madhumithavenu03@gmail.com>
2023-12-29 15:06:32 +0530

    adding icecream feature

--- rtk_demo/index.js
+++ rtk_demo/index.js
@@ -1,4 +1,5 @@
 const store = require('./app/store')
+const  icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
 
 console.log('Initial State ', store.getState())
@@ -9,5 +10,9 @@ store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.restocked(3))
+
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.restocked(2))
 
 unsubscribe()
