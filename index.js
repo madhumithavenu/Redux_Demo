4b6caa4f180b59ba20408d5f8d82d0f9fcac4322
Madhumitha <madhumithavenu03@gmail.com>
2023-12-29 11:32:37 +0530

    Restoking cakes

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 
 function orderCake() {
   return {
     type: CAKE_ORDERED,
-    quantity: 1,
+    payload: 1,
+  };
+}
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
   };
 }
 
@@ -20,6 +27,11 @@ const reducer = function (state = initialState, action) {
       return {
         ...state,
         numOfCakes: state.numOfCakes - 1,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
       };
     default:
       return state;
@@ -36,5 +48,6 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+store.dispatch(restockCake(3));
 
 unsubscribe();
