4cadb3abe296d62ea04f1489fe60a20e8c2ec760
Madhumitha <madhumithavenu03@gmail.com>
2024-01-04 12:22:23 +0530

    Async Thunks

--- rtk_demo/index.js
+++ rtk_demo/index.js
@@ -1,18 +1,22 @@
 const store = require('./app/store')
 const  icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
-
+const fetchUsers = require('./features/user/userSlice').fetchUsers
 
 console.log('Initial State ', store.getState())
 const unsubscribe = store.subscribe(() => {
+    console.log('Updated state', store.getState());
 })
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(3))
 
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.restocked(2))
+store.dispatch(fetchUsers())
 
-unsubscribe()
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(3))
+
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.restocked(2))
+
+// unsubscribe()
