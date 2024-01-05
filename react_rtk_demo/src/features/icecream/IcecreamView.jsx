dcf8cd47111fa1c130711b4deccd26a62f47c197
Madhumitha <madhumithavenu03@gmail.com>
2024-01-05 20:41:34 +0530

    using usedispatch for ice cream

--- react_rtk_demo/src/features/icecream/IcecreamView.jsx
+++ react_rtk_demo/src/features/icecream/IcecreamView.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { ordered, restocked } from './icecreamSlice.js'
 
 export const IcecreamView = () => {
+  const [value, setValue] = React.useState(1)
+  const numOfIcecreams = useSelector(state => state.icecream.numOfIcecreams)
+  const dispatch = useDispatch()
   return (
     <div>
-      <h2>Number of Ice Creams : </h2>
-      <button>Order Icecream</button>
-      <button>Restock Icecream</button>
+      <h2>Number of ice creams - {numOfIcecreams}</h2>
+      <button onClick={() => dispatch(ordered())}>Order Ice cream</button>
+      <input
+        type='number'
+        value={value}
+        onChange={e => setValue(parseInt(e.target.value))}
+      />
+      <button onClick={() => dispatch(restocked(value))}>
+        Restock Ice creams
+      </button>
     </div>
-  );
-};
+  )
+}
--- react_rtk_demo/src/features/icecream/icecreamSlice.js
+++ react_rtk_demo/src/features/icecream/icecreamSlice.js
@@ -1,27 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { ordered as cakeOrdered } from "../cake/cakeSlice";
+import { createSlice } from '@reduxjs/toolkit'
+import { ordered as cakeOrdered } from '../cake/cakeSlice'
 
 const initialState = {
-  numOfIcecreams: 20,
-};
+  numOfIcecreams: 10
+}
 
 const icecreamSlice = createSlice({
-  name: "icecream",
+  name: 'icecream',
   initialState,
   reducers: {
-    ordered: (state) => {
-      state.numOfIcecreams--;
+    ordered: state => {
+      state.numOfIcecreams--
     },
     restocked: (state, action) => {
-      state.numOfIcecreams += action.payload;
-    },
+      state.numOfIcecreams += action.payload
+    }
   },
-  extraReducers: (builder) => {
-    builder.addCase(cakeOrdered, (state) => {
-      state.numOfIcecreams--;
-    });
-  },
-});
+  extraReducers: builder => {
+    builder.addCase(cakeOrdered, state => {
+      state.numOfIcecreams--
+    })
+  }
+})
 
-export default icecreamSlice.reducer;
-export const { ordered, restocked } = icecreamSlice.actions;
+export default icecreamSlice.reducer
+export const { ordered, restocked } = icecreamSlice.actions
